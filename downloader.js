e45aa2c8c6e2c527092aecb365eda8151a8afd23
FOXYSER <101963772+FOXYSER@users.noreply.github.com>
2022-03-20 13:07:16 +0530

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -32,11 +32,11 @@ Asena.addCommand({ pattern: 'ig ?(.*)', fromMe: false, desc: IG_DESC}, async (me
         const msg = `${type}`
 
 	 if (msg === 'image') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
-          caption: "Made By Amalser💘"
+          caption: "Made By Foxy Ser💘"
         })}
 		 	 
 	if (msg === 'video') { await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-          caption: "Made By Amalser💘"
+          caption: "Made By Foxy Ser💘"
         })}
 	
         
@@ -71,7 +71,7 @@ Asena.addCommand({ pattern: 'fb ?(.*)', fromMe: false, desc: FBDESC }, async (me
         const msg = `*${CAPTION}*: ${judul}`
 
         await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.video, {
-          caption: "Made By Amalser 💘"
+          caption: "Made By Foxy Ser 💘"
         })
       })
       .catch(
